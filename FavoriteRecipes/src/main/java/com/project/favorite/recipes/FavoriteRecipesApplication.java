cebd5b2b8f5beecb2a767d4536d1e16f1197f658
sumit-vaidya <vaidyasumit007@gmail.com>
2021-05-28 20:09:52 +0530

    Added Sop statement

--- FavoriteRecipes/src/main/java/com/project/favorite/recipes/FavoriteRecipesApplication.java
+++ FavoriteRecipes/src/main/java/com/project/favorite/recipes/FavoriteRecipesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FavoriteRecipesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring boot");
 		SpringApplication.run(FavoriteRecipesApplication.class, args);
 	}
 
