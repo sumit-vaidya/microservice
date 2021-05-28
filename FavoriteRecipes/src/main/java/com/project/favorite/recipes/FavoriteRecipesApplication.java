8e229fc058f46da7aff53a642398d0e1f0e37153
sumit-vaidya <vaidyasumit007@gmail.com>
2021-05-28 20:07:08 +0530

    Initial Commit

---
+++ FavoriteRecipes/src/main/java/com/project/favorite/recipes/FavoriteRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.project.favorite.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteRecipesApplication.class, args);
+	}
+
+}
---
+++ FavoriteRecipes/src/test/java/com/project/favorite/recipes/FavoriteRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.favorite.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
